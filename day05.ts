997ef1c736e1b33694b31e9635ac41dcfb0bce2c
Drew Hirschi <drew@atlasai.tools>
2024-12-05 17:48:15 -0800

    day 5 part two complete

--- day05.ts
+++ day05.ts
@@ -1,3 +1,4 @@
+// const input = await Deno.readTextFile('day05.input.test.txt')
 const input = await Deno.readTextFile('day05.input.txt')
 
 
@@ -17,28 +18,43 @@ const updates = rawUpdates.split("\n").map(x => {
 })
 
 const validUpdates = []
+const invalidUpdates = []
 
 for (const update of updates) {
     let isValidUpdate = true
     for (let i = 0; i < update.length - 1; i++) {
-        const page = update[i]
+        let page = update[i]
         for (let j = i + 1; j < update.length; j++) {
             const checkPage = update[j]
             const checkRules = rules.get(checkPage)
             if (checkRules && checkRules!.some(rule => rule === page)) {
+                // console.log(`failed rule: ${checkPage}|${page}`, update);
+
+                [update[i], update[j]] = [update[j], update[i]];
+                page = update[i]
+                // console.log(`failed rule: ${checkPage}|${page}, =>`, update);
                 isValidUpdate = false
+                // break
             }
         }
     }
     if (isValidUpdate) {
         validUpdates.push(update)
+    } else {
+        invalidUpdates.push(update)
     }
 }
 
-const medians = validUpdates.map(update => {
+
+
+const medians = invalidUpdates.map(update => {
     const middle = Math.floor(update.length / 2)
     return update[middle]
 }).reduce((a, b) => a + Number(b), 0)
 
 console.log(medians)
+
+// console.log()
+
+
 
